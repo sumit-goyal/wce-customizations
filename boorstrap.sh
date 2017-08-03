ef7e936548d146f5f5d53cb9b91bfaec46c3383f
Sumit Goyal <sumitgoyal321@gmail.com>
2017-08-03 11:34:30 +0200

    Update bootstarp.sh
    Add R package installtion

--- boorstrap.sh
+++ boorstrap.sh
@@ -1,18 +1,16 @@
-sudo package-admin -c install -p gcc 	bzip2 \
-  cmake \
-	gcc-c++ \
-	gcc-gfortran \
-	maven \
-	tar \
-  protobuf-devel \
-  snappy-devel \
-  opencv-devel \
-  boost-devel \
-  make \
-  wget \
-  atlas-devel
+# Install OS libraries
+sudo package-admin -c install -p gcc-c++
 
+# Copy Scala Jars
+wget https://github.com/sumit-goyal/wce-customizations/blob/master/xgboost4j-0.7.jar -P /home/common/lib/scala/common
+wget https://github.com/sumit-goyal/wce-customizations/blob/master/xgboost4j-spark-0.7.jar -P /home/common/lib/scala/common
 
-  wget https://cran.r-project.org/src/base/R-2/R-2.15.3.tar.gz
-  tar zxvf R-2.15.3.tar.gz; cd R-2.15.3/
-  ./configure; make; sudo make install
+# Install Python 2 Packages
+/home/common/conda/anaconda2/bin/pip install costcla==0.5
+/home/common/conda/anaconda2/bin/pip install xgboost
+
+# Install Python3 Packages
+/home/common/conda/anaconda3/bin/pip install nltk==3.2.4
+
+# Install R apackages
+/usr/bin/R -e "install.packages(c('jsonlite','mime','openssl','curl','digest','R6','httr','memoise','whisker','rstudioapi','git2r','withr','devtools'), repos='https://cran.cnr.berkeley.edu', lib='/home/common/lib/R')"
